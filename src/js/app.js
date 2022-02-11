380e582c2b4d71dc569e679c11f5c3c4f8b2c2ae
Viseko <viseko23@gmail.com>
2022-02-11 14:39:15 +0700

    Вёрстка адаптива шапки

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,34 @@
 import * as flsFunctions from "./modules/functions.js";
+import {adaptiveMove} from "./modules/adaptive-move.js";
 
 // Запуск основных функций
 flsFunctions.isWebp();
+adaptiveMove();
+
+// Состояния страницы
+let isMenuOpened = false;
+
+// Мобильное меню
+const menuBtn = document.querySelector("#js-menu-btn");
+const menuCloseBtn = document.querySelector("#js-menu-close");
+
+function closeMenu() {
+  document.documentElement.classList.remove("_menu-open");
+  menuBtn.focus();
+  isMenuOpened = false;
+}
+
+function openMenu() {
+  document.documentElement.classList.add("_menu-open");
+  menuCloseBtn.focus();
+  isMenuOpened = true;
+}
+
+menuBtn.addEventListener("click", openMenu);
+menuCloseBtn.addEventListener("click", closeMenu);
+
+// Функции оверлея
+const overlay = document.querySelector("#js-page-overlay");
+overlay.onclick = () => {
+  if (isMenuOpened) closeMenu();
+};
