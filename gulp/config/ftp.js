b2c65281614469ea3cca2ee07bc7b6b33412d8c1
Viseko <viseko23@gmail.com>
2022-04-05 21:30:41 +0700

    Правка хедера

--- gulp/config/ftp.js
+++ gulp/config/ftp.js
@@ -1,6 +1,6 @@
 export let configFTP = {
-  host: "", // Адрес FTP-сервера
-  user: "", // Имя пользователя
-  password: "", // Пароль
+  host: "ftp://31.31.196.161/www/aspect-mg.ru", // Адрес FTP-сервера
+  user: "u1588513", // Имя пользователя
+  password: "m5V9kMK1a1mnCdL5", // Пароль
   parallel: 5, // Кол-во параллельных потоков
 };
